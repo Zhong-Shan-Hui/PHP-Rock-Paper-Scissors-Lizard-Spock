f7f8364029ce010680df603cc8f37cfeff23ec52
Zhong-Shan-Hui <71785870+Zhong-Shan-Hui@users.noreply.github.com>
2020-09-24 09:47:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Zhong Shan Hui 354ae7eb</title>
+<?php require_once "bootstrap.php"; 
+session_start();
+ ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
