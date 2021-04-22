e0ca494a10fe9c54efe3a0d514285494c2834fcb
Zhong-Shan-Hui <71785870+Zhong-Shan-Hui@users.noreply.github.com>
2021-04-22 19:28:14 +0530

    Update game.php

--- game.php
+++ game.php
@@ -11,38 +11,30 @@ if ( isset($_POST['logout']) ) {
     return;
 }
 // Set up the values for the game...
-// 0 is Rock, 1 is Paper, and 2 is Scissors
-$names = array('Rock', 'Paper', 'Scissors');
+// 0 is Rock, 1 is Paper, 2 is Scissors, 3 is Lizard and 4 is Spock
+$names = array('Rock', 'Paper', 'Scissors','Lizard','Spock');
 $human = isset($_POST["human"]) ? $_POST['human']+0 : -1;
-$computer = rand(0,2); // Hard code the computer to rock
-// TODO: Make the computer be random
-// $computer = rand(0,2);
+$computer = rand(0,4);
+
 // This function takes as its input the computer and human play
 // and returns "Tie", "You Lose", "You Win" depending on play
 // where "You" is the human being addressed by the computer
 function check($computer, $human) {
-    // For now this is a rock-savant checking function
-    // TODO: Fix this
-    if ( $human == $computer ) {
+    if (( $human - $computer )%5 == 0) {
         return "Tie";
-    } else if ( $human == 1 AND $computer == 0 ){
+    } else if ((( $human - $computer + 5)%5 == 1) OR (( $human - $computer + 5)%5 == 2)){
         return "You Win";
-    } else if ( $human == 2 AND $computer == 1 ){
-        return "You Win";
-    } else if ( $human == 0 AND $computer == 2 ){
-    	return "You Win";
-    } else if ( $human == 2  and $computer == 0 ) {
-        return "You Lose";
-    } else if ( $human == 1  and $computer == 2) {
-        return "You Lose";
-    } else if ( $human == 0  and $computer == 1 ) {
+    } else if ((( $human - $computer + 5)%5 == 3) OR (( $human - $computer + 5)%5 == 4)){
         return "You Lose";
     }
     
     return false;
 }
 // Check to see how the play happenned
-$result = check($computer, $human);
+if($human!=-1)
+{
+	$result = check($computer, $human);
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -53,7 +45,7 @@ $result = check($computer, $human);
 </head>
 <body>
 <div class="container">
-<h1>Rock Paper Scissors</h1>
+<h1>Rock Paper Scissors Lizard Spock</h1>
 <?php
 if ( isset($_REQUEST['name']) ) {
     echo "<p>Welcome: ";
@@ -67,7 +59,9 @@ if ( isset($_REQUEST['name']) ) {
 <option value="0">Rock</option>
 <option value="1">Paper</option>
 <option value="2">Scissors</option>
-<option value="3">Test</option>
+<option value="3">Lizard</option>
+<option value="4">Spock</option>
+<option value="5">Test</option>
 </select>
 <input type="submit" value="Play">
 <input type="submit" name="logout" value="Logout">
@@ -77,9 +71,9 @@ if ( isset($_REQUEST['name']) ) {
 <?php
 if ( $human == -1 ) {
     print "Please select a strategy and press Play.\n";
-} else if ( $human == 3 ) {
-    for($c=0;$c<3;$c++) {
-        for($h=0;$h<3;$h++) {
+} else if ( $human == 5 ) {
+    for($c=0;$c<5;$c++) {
+        for($h=0;$h<5;$h++) {
             $r = check($c, $h);
             print "Human=$names[$h] Computer=$names[$c] Result=$r\n";
         }
