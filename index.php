c0f09707c9e1b8f98e071f73eae91d9b330bf241
Zhong-Shan-Hui <71785870+Zhong-Shan-Hui@users.noreply.github.com>
2021-04-22 19:29:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 </head>
 <body>
 <div class="container">
-<h1>Welcome to Rock Paper Scissors</h1>
+<h1>Welcome to Rock Paper Scissors Lizard Spock</h1>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
